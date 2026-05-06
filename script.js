efc819d8557b0f654490644731a490edc9d8aa47
Lorteddy12 <lorteddy904@gmail.com>
2026-05-06 08:26:53 -0700

    w
    new

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-aboutButton.addEventListener("click", () => Scroll("AboutMe"));
-skillsButton.addEventListener("click", () => Scroll("Skills"));
-projectsButton.addEventListener("click", () => Scroll("Projects"));
 
-function Scroll(sectionId) {
-    let section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: "smooth" });
-}
+
+const projects = document.querySelectorAll('.proj');
+
+projects.forEach((proj) => {
+    // create overlay
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    proj.appendChild(overlay);
+
+    let text = proj.dataset.title;
+    let p = document.createElement("H2");
+    p.textContent = text;
+    overlay.appendChild(p);
+
+    // hover in
+    proj.addEventListener('mouseenter', () => {
+        overlay.style.opacity = '0';
+    });
+
+    // hover out
+    proj.addEventListener('mouseleave', () => {
+        overlay.style.opacity = '0.7';
+    });
+});
+
+projects.forEach((proj, index) => {
+    proj.addEventListener("click", () => {
+        window.location.href = `page${index + 1}.html`;
+    });
+});
+// This just creates another page, and i already made html pages with numerical order, which was easy.
